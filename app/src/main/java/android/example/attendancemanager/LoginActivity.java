6bc7977af302d9e162091f19514bded255926021
991rajat <991rajat@gmail.com>
2019-07-20 16:38:58 +0530

    Add Firebase Login Authentication and sign out

--- app/src/main/java/android/example/attendancemanager/LoginActivity.java
+++ app/src/main/java/android/example/attendancemanager/LoginActivity.java
@@ -1,11 +1,14 @@
 package android.example.attendancemanager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -13,15 +16,20 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
     private TextInputLayout email,password;
     TextView create;
-
+    private ProgressDialog progressDialog;
     private Button submit;
     Toolbar toolbar;
+    private FirebaseAuth mauth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +40,8 @@ public class LoginActivity extends AppCompatActivity {
         password = findViewById(R.id.login_password);
         submit = findViewById(R.id.login);
         create = findViewById(R.id.login_create);
-
+        progressDialog = new ProgressDialog(this);
+        mauth = FirebaseAuth.getInstance();
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Sign In");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -42,7 +51,29 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if(validate())
                 {
-                    Toast.makeText(LoginActivity.this, "VALID", Toast.LENGTH_SHORT).show();
+                    progressDialog.setTitle("Logging In");
+                    progressDialog.setMessage("Hold it ryt there sparky!");
+                    progressDialog.setCancelable(false);
+                    progressDialog.show();
+                    final String name = email.getEditText().getText().toString();
+                    final String pass = password.getEditText().getText().toString();
+                    mauth.signInWithEmailAndPassword(name,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful())
+                            {
+                                progressDialog.dismiss();
+                                Log.d("login",name+pass);
+                                Toast.makeText(LoginActivity.this, "Login Successfully", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(LoginActivity.this,MainActivity.class));
+                            }
+                            else
+                            {
+                                progressDialog.dismiss();
+                                Log.d("loginnot",name+pass);
+                            }
+                        }
+                    });
                 }
             }
         });
--- app/src/main/java/android/example/attendancemanager/MainActivity.java
+++ app/src/main/java/android/example/attendancemanager/MainActivity.java
@@ -1,18 +1,27 @@
 package android.example.attendancemanager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
 
+import static android.example.attendancemanager.R.menu.maintoolbar_menu;
+
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.maintoolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("YOO");
     }
 
 
@@ -29,4 +38,28 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(maintoolbar_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case R.id.signout:
+                signOut();break;
+        }
+        return true;
+    }
+
+    private void signOut() {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(MainActivity.this,LoginActivity.class));
+    }
+
 }
